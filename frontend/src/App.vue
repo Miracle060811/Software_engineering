1bf08489824d14caf6e561ffea0b80b7ae4ea726
YFan <2860314668@qq.com>
2026-04-13 19:40:27 +0800

    框架

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-config-provider>
+    <div id="app-container">
+      <router-view />
+    </div>
+  </el-config-provider>
+</template>
+
+<script setup>
+</script>
+
+<style>
+#app-container {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+}
+</style>
