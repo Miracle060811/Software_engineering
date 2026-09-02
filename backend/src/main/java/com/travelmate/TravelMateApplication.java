9a2a16cdfa1826d16914b613b97edd71eb539815
Zyy_mie <2052763747@qq.com>
2026-09-02 15:12:40 +0800

    答辩演示

--- backend/src/main/java/com/travelmate/TravelMateApplication.java
+++ backend/src/main/java/com/travelmate/TravelMateApplication.java
@@ -8,8 +8,7 @@ public class TravelMateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelMateApplication.class, args);
-        System.out.println("====== TravelMate Backend Started Successfully======");
-        
+        System.out.println("====== TravelMate Backend Started Successfully \n======");
     }
 
 }
